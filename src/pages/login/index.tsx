f4917f8c49de71ec68cbc43fc9452c3250e9b74c
Luis Miguel <lmcorrea@inf.ufsm.br>
2024-05-09 13:41:50 -0300

    wip

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,7 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
+
 import useLogin from "../../hooks/useLogin";
 import CustomButton from "../../components/Button";
 import CustomInput from "../../components/Input";
